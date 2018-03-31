a09ac030268b5b9a67b50fcbbfa266fb9a7b3ae7
Aiome <420454139@qq.com>
2018-03-31 13:35:33 +0800

    更新秒杀流程

--- curriculum-design-service/src/main/java/top/aiome/service/seckill/service/Processor.java
+++ curriculum-design-service/src/main/java/top/aiome/service/seckill/service/Processor.java
@@ -56,61 +56,45 @@ public class Processor {
     /**
      * 获取库存情况，将Mysql库存刷到Redis
      * @param seckillId
-     * @param m
+     * @param m  0:无库存 1：有库存 2：秒杀id不存在
      * @return
      */
     private int checkReminds(Integer seckillId,Long m) {
-        //0:无库存 1：有库存 2：秒杀id不存在
-        int reminds = 1;
-
-        //从Redis检查数据，不必完全严格检查
-        long t1 = System.currentTimeMillis();
+        int reminds = 1;    //从Redis检查数据，不必完全严格检查
         boolean hasKey = redisTemplate.hasKey(generateRedisKey(seckillId));
         boolean hasKeyLoc = redisTemplate.hasKey(generateRedisKeyLoc(seckillId));
-        long t2 = System.currentTimeMillis();
-
-
+        //redis库存已经初始化
         if (hasKey){
             Long count = redisTemplate.opsForList().size(generateRedisKey(seckillId));
             reminds = count < 1 ? 0 : 1;
             logger.info(m + "---redis 库存情况:" + count);
-        }else if(!hasKeyLoc){
-            //如果redis无缓存则查mysql
-
+        }else if(!hasKeyLoc){ //如果redis库存未初始化并且没有锁
+            //校验商品有效性
             Seckill seckillById = seckillSVImpl.getSeckillById(seckillId);
             logger.info(m + "---mysql 库存情况:" + seckillById.getSeckillCount());
-
-            if (seckillById == null){
-                return 2;
-            }
+            if (seckillById == null){ return 2; }
             reminds = seckillById.getSeckillCount() < 1 ?  0 : 1;
-
-            //将库存存入redis
+            //将库存存入redis  并且线程同步
             synchronized (this){
                 boolean hasLoc = redisTemplate.hasKey(generateRedisKeyLoc(seckillId));
                 if (hasLoc){
                     return redisTemplate.opsForList().size(generateRedisKey(seckillId)) > 0 ? 1 : 0;
                 }
+                //redis加锁并且将库存刷入
                 redisTemplate.opsForList().leftPush(generateRedisKeyLoc(seckillId),1);
-
                 List<Object> objects = redisTemplate.executePipelined(new RedisCallback<Object>() {
-                    @Override
+                    @Override//redis管道刷库存数据
                     public Object doInRedis(RedisConnection connection) throws DataAccessException {
-                        long t1 = System.currentTimeMillis();
                         connection.openPipeline();
-
                         for (int i = 1; i <= seckillById.getSeckillCount(); i ++){
                             redisTemplate.opsForList().leftPush(generateRedisKey(seckillId),i);
                         }
-
                         connection.closePipeline();
-                        long t2 = System.currentTimeMillis();
-                        logger.info( m + "---管道:" + (t2 - t1));
                         return null;
                     }
                 });
             }
-        }else if (hasKeyLoc){
+        }else if (hasKeyLoc){//如果redis没有库存信息但是又锁
             reminds = 0;
         }
         return reminds;
@@ -124,13 +108,10 @@ public class Processor {
     public int preProcess(SeckillInfo info){
         Long m = System.currentTimeMillis();
         int result = checkReminds(info.getSeckillId(),m);
-        if (1 == result) {
-
+        if (1 == result) {//有库存
              //原子操作扣减Redis缓存库存
              Integer value = redisTemplate.opsForList().rightPop(generateRedisKey(info.getSeckillId()));
              logger.info(m + "---减redis库存返回值：" + value);
-
-
             //开启事务进行减库存下单操作
             if (null != value && value >= 0){
                 logger.info( m + "---redis 出队：successful");
@@ -138,38 +119,28 @@ public class Processor {
                 seckill.setId(info.getSeckillId());
                 //扣库存
                 boolean reduceInventory = seckillSVImpl.reduceInventory(seckill);
-
-
-                if (!reduceInventory){
-                    logger.info( m + "---扣减库存：失败" );
-                }
-
+                if (!reduceInventory){  logger.info( m + "---扣减库存：失败" );    }
                 logger.info( m + "---扣减库存：成功" );
-
                 //下单
                 Order order = new Order();
                 order.setTypeId(info.getSeckillId());
                 order.setCreateTime(new Date());
                 order.setUserId(info.getUserId());
-
                 int insertOrder = orderSVImpl.insertOrder(order);
                 if (insertOrder < 1){
                     logger.info( m + "---下单操作：失败" );
                     throw new RuntimeException("下单操作：失败" );
                 }
                 logger.info( m + "---下单操作：成功" );
-
                 return  Constants.Seckill.SECKILL_SUCCESS;
             }
-
             return Constants.Seckill.SECKILL_FAIL;
-        }else if (2 == result){
+        }else if (2 == result){//秒杀不存在
             return Constants.Seckill.SECKILL_ILLEGAL_ID;
-        }else if (0 == result){
+        }else if (0 == result){//无库存
             logger.info(m + "---无库存");
             return Constants.Seckill.SECKILL_FAIL;
         }
-
         return Constants.Seckill.SECKILL_FAIL;
     }
 }
---
+++ curriculum-design-service/src/main/java/top/aiome/service/seckill/service/test.java
@@ -0,0 +1,12 @@
+package top.aiome.service.seckill.service;
+
+/**
+ * Created by mahongyan on 2018/3/29
+ */
+public class test {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50000; i++) {
+            System.out.println("8784,"+i);
+        }
+    }
+}
